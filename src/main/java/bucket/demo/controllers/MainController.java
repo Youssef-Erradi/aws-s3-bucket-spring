15eb758e700c31b59378b2b5f9d79d728dff485b
Youssef Erradi <74113158+Youssef-Erradi@users.noreply.github.com>
2022-08-03 13:12:50 +0100

    use UserService instead of UserRepository

--- src/main/java/bucket/demo/controllers/MainController.java
+++ src/main/java/bucket/demo/controllers/MainController.java
@@ -3,19 +3,21 @@ package bucket.demo.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import bucket.demo.dtos.UserDTO;
 import bucket.demo.entities.User;
-import bucket.demo.repositories.UserRepository;
+import bucket.demo.services.UserService;
 import lombok.AllArgsConstructor;
 
 @Controller
 @AllArgsConstructor
 public class MainController {
-	private UserRepository repository;
+	private UserService service;
 	
 	@GetMapping
 	public String index(Model model) {
-		model.addAttribute("users", repository.findAll());
+		model.addAttribute("users", service.findAll());
 		return "list";
 	}
 	
@@ -25,4 +27,9 @@ public class MainController {
 		return "form";
 	}
 	
+	@PostMapping("/add")
+	public String save(UserDTO user) {
+		service.save(user);
+		return "redirect:/";
+	}
 }
