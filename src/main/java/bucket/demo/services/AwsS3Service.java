15aaf99b55a2b0900075f681dc1630acdef510ac
Youssef Erradi <74113158+Youssef-Erradi@users.noreply.github.com>
2022-08-03 13:08:33 +0100

    allow public read of bucket objects

--- src/main/java/bucket/demo/services/AwsS3Service.java
+++ src/main/java/bucket/demo/services/AwsS3Service.java
@@ -3,6 +3,7 @@ package bucket.demo.services;
 import java.io.IOException;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
@@ -10,31 +11,33 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.SdkClientException;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 
-import lombok.AllArgsConstructor;
-
 @Service
-@AllArgsConstructor
-public class AwsS3Service implements FileService{
+public class AwsS3Service implements FileService {
+	@Autowired
 	private AmazonS3Client s3Client;
-	
+
 	@Value("${s3.bucket.name}")
 	private String bucketName;
-	
+
 	@Override
 	public String upload(MultipartFile file) {
-		String filename = UUID.randomUUID().toString() + "." +StringUtils.getFilenameExtension(file.getOriginalFilename());
+		String filename = UUID.randomUUID().toString() + "."
+				+ StringUtils.getFilenameExtension(file.getOriginalFilename());
 		ObjectMetadata metaData = new ObjectMetadata();
 		metaData.setContentLength(file.getSize());
 		metaData.setContentType(file.getContentType());
-		
+
 		try {
 			s3Client.putObject(bucketName, filename, file.getInputStream(), metaData);
 		} catch (SdkClientException | IOException e) {
 			e.printStackTrace();
 		}
 		
+		s3Client.setObjectAcl(bucketName, filename, CannedAccessControlList.PublicRead);
+
 		return s3Client.getResourceUrl(bucketName, filename);
 	}
 
